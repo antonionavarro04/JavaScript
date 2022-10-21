970b9a5391d39288194eb5284b2ff4352dd2a15b
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-10-21 09:19:39 +0200

    Obrigado Joaquin

--- MiPrimeraPagina/js/sript.js
+++ MiPrimeraPagina/js/sript.js
@@ -1,5 +1,4 @@
 let num = 0;
-String numStg = "resultado";
 
 boton = document.getElementById("boton");
 mostrador = document.getElementById("mostrador");
@@ -7,6 +6,5 @@ mostrador = document.getElementById("mostrador");
 function suma(){
     num++;
     console.warn("Valor incrementado en 1, total: " + num);
-    // Hacer que el recuadro mostrador se actualice con el valor de num
-    mostrador.innerHTML = num;
+    mostrador.value = num;
 }
