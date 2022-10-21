486db70f975f950766dc9dd01eb162fd075852e7
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-10-21 10:55:58 +0200

    num == 3

--- MiPrimeraPagina/js/sript.js
+++ MiPrimeraPagina/js/sript.js
@@ -2,9 +2,14 @@ let num = 0;
 
 boton = document.getElementById("boton");
 mostrador = document.getElementById("mostrador");
+win = document.getElementById("win");
 
 function suma(){
     num++;
     console.warn("Valor incrementado en 1, total: " + num);
     mostrador.value = num;
+
+    if (num == 3){
+        win.innerHTML = "¡Has ganado!";
+    }
 }
